0b58b67ea54c5d6fe3da62309e9cdcaa210055f7
Vignesh Ravichandran <vigneshravichanders@gmail.com>
2020-03-08 01:00:09 +0530

    Updates

---
+++ precache-manifest.805a595c3dcd108ff474c60f0bc73b46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b6be168ad051479bbd79bce5ee4d9a",
+    "url": "/React-Snake-Game/index.html"
+  },
+  {
+    "revision": "43a71b70729067af12c8",
+    "url": "/React-Snake-Game/static/css/main.b95bcbb4.chunk.css"
+  },
+  {
+    "revision": "658cf560c87e81623d10",
+    "url": "/React-Snake-Game/static/js/2.e97c1861.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React-Snake-Game/static/js/2.e97c1861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a71b70729067af12c8",
+    "url": "/React-Snake-Game/static/js/main.7aa605fd.chunk.js"
+  },
+  {
+    "revision": "62537e515bff336f7784",
+    "url": "/React-Snake-Game/static/js/runtime-main.86b6cbec.js"
+  }
+]);
